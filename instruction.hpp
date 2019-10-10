425508a4f94ef75e69f07106644df225edcb3566
Kai Zhang <kazhang@sonicwall.com>
2019-10-10 19:23:19 +0800

    change

--- instruction.hpp
+++ instruction.hpp
@@ -1,8 +1,8 @@
 #ifndef INSTRUCTION_HPP
 #define INSTRUCTION_HPP
-
+#include <iostream>
 #include <cstdint>    //using
-
+#include<bitset>
 using word = uint32_t;
 
 class instruction{
@@ -14,7 +14,7 @@ public:
   int i_imi;
   int j_add;
   char type;
-
+  std::bitset<32> bin;
   instruction();
   instruction(word inst);
 private:
@@ -22,5 +22,11 @@ private:
   bool is_I_type(int opcode);
   bool is_J_type(int opcode);
 };
+
+std::ostream& operator<<(std::ostream&  os,const instruction& instr)
+{
+	os << "opcode:" << "src_s:" << "src_t:" << "destn:"<<"shamt:" <<"funct:"<< "i_imi"<<"j_add"<< "type";
+}
+
 
 #endif
